ecab32580e85e0251464c477e323f0ae5f2ada53
Arkadiy <poisonoff@gmail.com>
2025-12-01 10:21:06 +0500

    sp-019 Added vehicle plate to UI (#18)

--- src/parking/ui/rest/response/vehicle.py
+++ src/parking/ui/rest/response/vehicle.py
@@ -1,5 +1,7 @@
 from kernel.ui.rest.base.response import BaseResponse
 
+from parking.ui.rest.response.plate import PlateResponse
+
 class VehicleDetailsResponse(BaseResponse):
     type: str
     color: str
@@ -7,3 +9,4 @@ class VehicleDetailsResponse(BaseResponse):
 
 class VehicleResponse(BaseResponse):
     details: VehicleDetailsResponse
+    plate: PlateResponse | None
